77e503980996dff6de0c9806127e4eb75a051a71
ilyabulya <ilyade3004@gmail.com>
2025-03-24 00:34:01 +0100

    ma

--- app/main.py
+++ app/main.py
@@ -10,10 +10,22 @@ def main():
         path = request.split(" ")[1]
 
         if path == "/":
-            client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        elif path.startswith("/echo/"):
+            # Получаем текст после "/echo/"
+            echo_str = path[len("/echo/"):]
+            content_length = len(echo_str.encode())  # учитываем байты, не только символы
+            response = (
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/plain\r\n"
+                f"Content-Length: {content_length}\r\n"
+                "\r\n"
+                f"{echo_str}"
+            )
         else:
-            client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
+        client.sendall(response.encode())
         client.close()
 
 
