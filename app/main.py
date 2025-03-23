12e33e3490bd40a6ad5e6ab3261e2fa766f8d95a
ilyabulya <ilyade3004@gmail.com>
2025-03-24 00:32:42 +0100

    ma

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
-import socket  # noqa: F401
+import socket
 
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
-    client, _ = server_socket.accept()
-    client_msg = client.recv(4096).decode().split(" ")
-    if client_msg[1] == "/":
-        client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    else:
-        client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    while True:
+        client, _ = server_socket.accept()
+        request = client.recv(4096).decode()
+        path = request.split(" ")[1]
+
+        if path == "/":
+            client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        else:
+            client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+        client.close()
+
 
 if __name__ == "__main__":
     main()
